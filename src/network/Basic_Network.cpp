f05bbb4b1feee7d95854aa643c3796300c52bc0b
Ahmad Bamba <ahmad.eb.bamba@gmail.com>
2016-03-13 19:13:22 -0400

    Travis CI version borked

--- src/network/Basic_Network.cpp
+++ src/network/Basic_Network.cpp
@@ -24,7 +24,7 @@ namespace Mist{
                 tcp::resolver::query query(server_name, "daytime");
                 tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
                 tcp::socket socket(io_service);
-                boost::asio::detail::socket_ops::connect(socket, endpoint_iterator);
+                boost::asio::connect(socket, endpoint_iterator);
 
                 std::cout << "Info: Client listening" << std::endl;
 
