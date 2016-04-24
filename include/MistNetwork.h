3bff8d9fa7bd2385fbf1dcb113c81e2457092d31
DavidFiumano <Mynameisspam1@gmail.com>
2016-04-24 19:30:51 -0400

    Added Cereal to the repo Also added files for Deserialize and Serialize

---
+++ include/MistNetwork.h
@@ -0,0 +1,11 @@
+#include <Mist.h>
+#include <boost/asio.hpp>
+
+class MistNetwork :
+	public Mist
+{
+private:
+	
+public:
+	
+}
--- include/Server_Socket.h
+++ include/Server_Socket.h
@@ -7,6 +7,9 @@ class Server_Socket{
 public:
     Server_Socket(boost::asio::io_service io_service, int port);
     boost::asio::io_service getIOService();
+    
+    void send();
+    /*serialized container*/ void recieve();
 private:
     boost::asio::io_service ios;
     int io_port;
